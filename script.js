ed281d30be1f762dabbd84445c17f75ce7162336
IrtizaSarwar <irtizasarwar2011@gmail.com>
2024-12-31 06:17:24 -0900

    Date added

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const clockElement = document.getElementById("clock");
+    const dateElement = document.getElementById("date");
 
-    function updateClock(){
+    function updateClock() {
         const now = new Date();
         let hours = now.getHours();
-        const minutes = String(now.getMinutes()).padStart(2, "0")
-        const seconds = String(now.getSeconds()).padStart(2, "0")
-
-        const amPM = hours >= 12? "PM" : "AM";
-
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+        const amPM = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12;
-
-        clockElement.textContent = `${String(hours).padStart(2, "0")} : ${minutes} : ${seconds} ${amPM}`
+        clockElement.textContent = `${String(hours).padStart(2, "0")}:${minutes}:${seconds} ${amPM}`;
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, "0");
+        const day = String(now.getDate()).padStart(2, "0");
+        dateElement.textContent = `${year}-${month}-${day}`;
     }
+
     setInterval(updateClock, 1000);
-
     updateClock();
-})
-
+});
